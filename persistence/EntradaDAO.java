32f2a5f6d49f0ebf0e2af295a03f329d3895dfbe
TassioTeodoro <140447407+TassioTeodoro@users.noreply.github.com>
2024-06-10 21:19:04 -0300

    Update EntradaDAO.java

--- persistence/EntradaDAO.java
+++ persistence/EntradaDAO.java
@@ -1,5 +1,7 @@
 package br.unigran.persistence;
 
+import br.unigran.entidades.Entrada;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
